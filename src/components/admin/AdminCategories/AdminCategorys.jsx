32df3b9e512ed2f1dde7581382df9c0d2f16ee45
noelreissig <NREISSIG@GMAIL.COM>
2021-09-04 16:01:34 -0700

    tildes y signos

--- src/components/admin/AdminCategories/AdminCategorys.jsx
+++ src/components/admin/AdminCategories/AdminCategorys.jsx
@@ -13,162 +13,161 @@ import ToastCannotDeleteCategory from "../../ToastCannotDeleteCategory/ToastCann
 import ToastProducto from "../../ToastProducto/ToastProducto";
 
 function AdminCategorys() {
-	const { token } = useSelector((state) => state.authReducer);
-	const [categories, setCategories] = useState([]);
-	const [show, setShow] = useState(false); //prende modal
-	const [showOk, setShowOk] = useState(false); //prende toasty
-	const [showCannotDelete, setShowCannotDelete] = useState(false);
+  const { token } = useSelector((state) => state.authReducer);
+  const [categories, setCategories] = useState([]);
+  const [show, setShow] = useState(false); //prende modal
+  const [showOk, setShowOk] = useState(false); //prende toasty
+  const [showCannotDelete, setShowCannotDelete] = useState(false);
 
-	const [refresh, setRefresh] = useState(false);
-	const [category, setCategory] = useState({});
+  const [refresh, setRefresh] = useState(false);
+  const [category, setCategory] = useState({});
 
-	const [newCategory, setNewCategory] = useState({});
-	const [showCreate, setShowCreate] = useState(false);
+  const [newCategory, setNewCategory] = useState({});
+  const [showCreate, setShowCreate] = useState(false);
 
-	const handleShow = () => {
-		setShow(true);
-	};
+  const handleShow = () => {
+    setShow(true);
+  };
 
-	const handleShowCreate = () => {
-		setShowCreate(true);
-	};
+  const handleShowCreate = () => {
+    setShowCreate(true);
+  };
 
-	// Todas las categorías
-	useEffect(() => {
-		const getCategories = async () => {
-			const response = await axios({
-				method: "get",
-				url: `${process.env.REACT_APP_API_URL}/api/category`,
-				headers: {
-					Authorization: `Bearer ${token}`,
-				},
-			});
-			setCategories(response.data);
-			setRefresh(false);
-		};
-		getCategories();
-	}, [refresh]);
+  // Todas las categorías
+  useEffect(() => {
+    const getCategories = async () => {
+      const response = await axios({
+        method: "get",
+        url: `${process.env.REACT_APP_API_URL}/api/category`,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setCategories(response.data);
+      setRefresh(false);
+    };
+    getCategories();
+  }, [refresh]);
 
-	// Eliminar categoría
-	async function handleDelete(id) {
-		try {
-			await axios.delete(`${process.env.REACT_APP_API_URL}/api/category/${id}`, {
-				headers: {
-					Authorization: `Bearer ${token}`,
-				},
-			});
-			setCategories((categories) =>
-				categories.filter((category) => category.id !== id)
-			);
-			setShowOk(true);
-		} catch (error) {
-			setShowCannotDelete(true);
-		}
-	}
+  // Eliminar categoría
+  async function handleDelete(id) {
+    try {
+      await axios.delete(
+        `${process.env.REACT_APP_API_URL}/api/category/${id}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      setCategories((categories) =>
+        categories.filter((category) => category.id !== id)
+      );
+      setShowOk(true);
+    } catch (error) {
+      setShowCannotDelete(true);
+    }
+  }
 
-	return (
-		<div>
-			<div className={adminCategory.imgBackground}>
-				<div className="container min-vh-100 w-auto">
-					<h2 className={`${adminCategory.admin} text-center text-white`}>
-						Gestión de Categorías
-					</h2>
-					<Link to="/admin" className="text-decoration-none">
-						<button className="btn btn-success d-block d-sm-none mx-auto mb-2">
-							Volver a Menú
-						</button>
-					</Link>
+  return (
+    <div>
+      <div className={adminCategory.imgBackground}>
+        <div className="container min-vh-100 w-auto">
+          <h2 className={`${adminCategory.admin} text-center text-white`}>
+            Gestión de Categorías
+          </h2>
+          <Link to="/admin" className="text-decoration-none">
+            <button className="btn btn-success d-block d-sm-none mx-auto mb-2">
+              Volver a Menú
+            </button>
+          </Link>
 
-					<div className="d-flex justify-content-center">
-						<button
-							className="btn btn-success mb-3 "
-							onClick={() => {
-								handleShowCreate();
-								setNewCategory(newCategory);
-							}}
-						>
-							Agregar categoría
-						</button>
-						<AdminCreateCategory
-							newCategory={newCategory}
-							showCreate={showCreate}
-							setShowCreate={setShowCreate}
-							setRefresh={setRefresh}
-							showOk={showOk}
-							setShowOk={setShowOk}
-						/>
-					</div>
+          <div className="d-flex justify-content-center">
+            <button
+              className="btn btn-success mb-3 "
+              onClick={() => {
+                handleShowCreate();
+                setNewCategory(newCategory);
+              }}
+            >
+              Agregar categoría
+            </button>
+            <AdminCreateCategory
+              newCategory={newCategory}
+              showCreate={showCreate}
+              setShowCreate={setShowCreate}
+              setRefresh={setRefresh}
+              showOk={showOk}
+              setShowOk={setShowOk}
+            />
+          </div>
 
-					<div className="row px-0">
-						<div className="col-md-3 w-auto d-none d-lg-block">
-							<SiderAdmin />
-						</div>
-						<div className="col-md-9">
-							<div
-								className={`${tableStyles.font} table-responsive-md bg-white`}
-							>
-								<Table striped bordered hover>
-									<thead>
-										<tr className="text-center">
-											<th>Id</th>
-											<th>Categorias</th>
-											<th>Editar</th>
-											<th>Borrar</th>
-										</tr>
-									</thead>
-									<tbody>
-										{categories.map((category) => (
-											<tr className="text-center">
-												<td className="text-center">
-													{category.id}
-												</td>
-												<td className="text-center">
-													{category.name}
-												</td>
-												<td>
-													<i
-														onClick={() => {
-															handleShow();
-															setCategory(category);
-														}}
-														className="fas fa-edit text-success"
-													></i>
-												</td>
-												<td>
-													<button className="btn m-0 p-0">
-														<i
-															onClick={() => {
-																handleDelete(category.id);
-															}}
-															className="far fa-trash-alt text-danger"
-														></i>
-													</button>
-												</td>
-											</tr>
-										))}
-										<AdminEditCategory
-											category={category}
-											show={show}
-											setShow={setShow}
-											setRefresh={setRefresh}
-											key={category.name}
-											showOk={showOk}
-											setShowOk={setShowOk}
-										/>
-									</tbody>
-								</Table>
-								<ToastProducto show={showOk} setShow={setShowOk} />
-								<ToastCannotDeleteCategory
-									show={showCannotDelete}
-									setShow={setShowCannotDelete}
-								/>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+          <div className="row px-0">
+            <div className="col-md-3 w-auto d-none d-lg-block">
+              <SiderAdmin />
+            </div>
+            <div className="col-md-9">
+              <div
+                className={`${tableStyles.font} table-responsive-md bg-white`}
+              >
+                <Table striped bordered hover>
+                  <thead>
+                    <tr className="text-center">
+                      <th>Id</th>
+                      <th>Categorías</th>
+                      <th>Editar</th>
+                      <th>Borrar</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {categories.map((category) => (
+                      <tr className="text-center">
+                        <td className="text-center">{category.id}</td>
+                        <td className="text-center">{category.name}</td>
+                        <td>
+                          <i
+                            onClick={() => {
+                              handleShow();
+                              setCategory(category);
+                            }}
+                            className="fas fa-edit text-success"
+                          ></i>
+                        </td>
+                        <td>
+                          <button className="btn m-0 p-0">
+                            <i
+                              onClick={() => {
+                                handleDelete(category.id);
+                              }}
+                              className="far fa-trash-alt text-danger"
+                            ></i>
+                          </button>
+                        </td>
+                      </tr>
+                    ))}
+                    <AdminEditCategory
+                      category={category}
+                      show={show}
+                      setShow={setShow}
+                      setRefresh={setRefresh}
+                      key={category.name}
+                      showOk={showOk}
+                      setShowOk={setShowOk}
+                    />
+                  </tbody>
+                </Table>
+                <ToastProducto show={showOk} setShow={setShowOk} />
+                <ToastCannotDeleteCategory
+                  show={showCannotDelete}
+                  setShow={setShowCannotDelete}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default AdminCategorys;
--- src/components/admin/SiderAdmin.jsx
+++ src/components/admin/SiderAdmin.jsx
@@ -5,108 +5,105 @@ import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 
 function SiderAdmin() {
-	const dispatch = useDispatch();
-	const history = useHistory();
-	const handleLogout = () => {
-		dispatch({ type: "LOGOUT_REQUEST" });
-		history.push("/login");
-	};
-	return (
-		<div className={`d-flex align-items-center`}>
-			<div className="row">
-				<div className="col-md-3 w-auto text-center">
-					<div
-						className={`${siderAdminStyles.borderBox} ms-2 mb-3 p-1 rounded shadow border-white`}
-					>
-						<Link to="/admin" className={`${siderAdminStyles.Link}`}>
-							<span className={`${siderAdminStyles.subtitulo} text-white`}>
-								Home
-								<i
-									className={`${siderAdminStyles.iconos} fas fa-home ps-2 `}
-								></i>
-							</span>
-						</Link>
-					</div>
-					<div
-						className={`${siderAdminStyles.borderBox} ms-2 mb-3 p-1 rounded shadow border-white`}
-					>
-						<Link
-							to="/admin/categorias"
-							className={`${siderAdminStyles.Link}`}
-						>
-							<span className={`${siderAdminStyles.subtitulo} text-white `}>
-								Categorias{" "}
-								<i
-									className={`${siderAdminStyles.iconos} fas fa-layer-group ps-2`}
-								></i>
-							</span>
-						</Link>
-					</div>
-					<div
-						className={`${siderAdminStyles.borderBox} ms-2 mb-3 p-1 rounded shadow border-white`}
-					>
-						<Link to="/admin/productos" className={siderAdminStyles.Link}>
-							<span className={`${siderAdminStyles.subtitulo} text-white`}>
-								Productos
-								<i
-									className={`${siderAdminStyles.iconos} fas fa-tags ps-2`}
-								></i>
-							</span>
-						</Link>
-					</div>
-					<div
-						className={`${siderAdminStyles.borderBox} ms-2 mb-3 p-1 rounded shadow border-white`}
-					>
-						<Link to="/admin/ordenes" className={siderAdminStyles.Link}>
-							<span className={`${siderAdminStyles.subtitulo} text-white`}>
-								Ordenes
-								<i
-									className={`${siderAdminStyles.iconos} fas fa-shopping-cart ps-2`}
-								></i>
-							</span>
-						</Link>
-					</div>
-					<div
-						className={`${siderAdminStyles.borderBox} ms-2 mb-3 p-1 rounded shadow border-white`}
-					>
-						<Link to="/admin/clients" className={siderAdminStyles.Link}>
-							<span className={`${siderAdminStyles.subtitulo} text-white`}>
-								Clientes
-								<i
-									className={`${siderAdminStyles.iconos} fas fa-user ps-2`}
-								></i>
-							</span>
-						</Link>
-					</div>
-					<div
-						className={`${siderAdminStyles.borderBox} ms-2 mb-3 p-1 rounded shadow border-white`}
-					>
-						<Link to="/admin/users" className={siderAdminStyles.Link}>
-							<span className={`${siderAdminStyles.subtitulo} text-white`}>
-								Admin
-								<i
-									className={`${siderAdminStyles.iconos} fas fa-users-cog ps-2`}
-								></i>
-							</span>
-						</Link>
-					</div>
-					<div
-						className={`${siderAdminStyles.borderBox} ms-2 mb-3 p-1 rounded shadow border-white`}
-					>
-						<Link
-							to="/admin"
-							className={siderAdminStyles.Link}
-							onClick={() => handleLogout()}
-						>
-							<span className={`${siderAdminStyles.subtitulo} text-white`}>
-								Logout
-							</span>
-						</Link>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const handleLogout = () => {
+    dispatch({ type: "LOGOUT_REQUEST" });
+    history.push("/login");
+  };
+  return (
+    <div className={`d-flex align-items-center`}>
+      <div className="row">
+        <div className="col-md-3 w-auto text-center">
+          <div
+            className={`${siderAdminStyles.borderBox} ms-2 mb-3 p-1 rounded shadow border-white`}
+          >
+            <Link to="/admin" className={`${siderAdminStyles.Link}`}>
+              <span className={`${siderAdminStyles.subtitulo} text-white`}>
+                Home
+                <i
+                  className={`${siderAdminStyles.iconos} fas fa-home ps-2`}
+                ></i>
+              </span>
+            </Link>
+          </div>
+          <div
+            className={`${siderAdminStyles.borderBox} ms-2 mb-3 p-1 rounded shadow border-white`}
+          >
+            <Link to="/admin/categorias" className={`${siderAdminStyles.Link}`}>
+              <span className={`${siderAdminStyles.subtitulo} text-white`}>
+                Categorías{" "}
+                <i
+                  className={`${siderAdminStyles.iconos} fas fa-layer-group ps-2`}
+                ></i>
+              </span>
+            </Link>
+          </div>
+          <div
+            className={`${siderAdminStyles.borderBox} ms-2 mb-3 p-1 rounded shadow border-white`}
+          >
+            <Link to="/admin/productos" className={siderAdminStyles.Link}>
+              <span className={`${siderAdminStyles.subtitulo} text-white`}>
+                Productos
+                <i
+                  className={`${siderAdminStyles.iconos} fas fa-tags ps-2`}
+                ></i>
+              </span>
+            </Link>
+          </div>
+          <div
+            className={`${siderAdminStyles.borderBox} ms-2 mb-3 p-1 rounded shadow border-white`}
+          >
+            <Link to="/admin/ordenes" className={siderAdminStyles.Link}>
+              <span className={`${siderAdminStyles.subtitulo} text-white`}>
+                Órdenes
+                <i
+                  className={`${siderAdminStyles.iconos} fas fa-shopping-cart ps-2`}
+                ></i>
+              </span>
+            </Link>
+          </div>
+          <div
+            className={`${siderAdminStyles.borderBox} ms-2 mb-3 p-1 rounded shadow border-white`}
+          >
+            <Link to="/admin/clients" className={siderAdminStyles.Link}>
+              <span className={`${siderAdminStyles.subtitulo} text-white`}>
+                Clientes
+                <i
+                  className={`${siderAdminStyles.iconos} fas fa-user ps-2`}
+                ></i>
+              </span>
+            </Link>
+          </div>
+          <div
+            className={`${siderAdminStyles.borderBox} ms-2 mb-3 p-1 rounded shadow border-white`}
+          >
+            <Link to="/admin/users" className={siderAdminStyles.Link}>
+              <span className={`${siderAdminStyles.subtitulo} text-white`}>
+                Admin
+                <i
+                  className={`${siderAdminStyles.iconos} fas fa-users-cog ps-2`}
+                ></i>
+              </span>
+            </Link>
+          </div>
+          <div
+            className={`${siderAdminStyles.borderBox} ms-2 mb-3 p-1 rounded shadow border-white`}
+          >
+            <Link
+              to="/admin"
+              className={siderAdminStyles.Link}
+              onClick={() => handleLogout()}
+            >
+              <span className={`${siderAdminStyles.subtitulo} text-white`}>
+                Logout
+              </span>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default SiderAdmin;
